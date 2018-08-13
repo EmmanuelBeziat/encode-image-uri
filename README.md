03d9b47e2ed7ccc4bc6336fd302ae06f3c549c4a
Emmanuel Béziat <contact@emmanuelbeziat.com>
2018-08-14 01:10:43 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # encode-image-uri
+
 Simple plugin to encode images to base64.
+
+* Allow to select mimetype between `image/png` (by default), `image/jpeg`, or `image/gif`
+
+## Installation
+
+```bash
+# npm
+$ npm i encode-image-uri
+
+# yarn
+$ yarn add encode-image-uri
+```
+
+## How to use
+
+```javascript
+const encodeImageURI = require('encode-image-uri')
+
+encodeImageURI('path/to/image', (error, uri) => {
+	if (!error) {
+		// Do stuff
+		console.log(uri)
+	}
+	else {
+		// Handle error
+		console.error(error)
+	}
+})
+```
---
+++ encode-image-uri.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const mimes = {
+	'png': 'image/png',
+	'gif': 'image/gif',
+	'jpg': 'image/jpeg',
+	'jpeg': 'image/jpeg',
+	'bm': 'image/bmp',
+	'bmp': 'image/bmp',
+	'webp': 'image/webp',
+	'ico': 'image/x-icon',
+	'svg': 'image/svg+xml'
+}
+
+module.exports = (url, callback) => {
+	const canvas = document.createElement('canvas')
+	const image = document.createElement('img')
+
+	image.addEventListener('load', () => {
+		const ctx = canvas.getContext('2d')
+		const contentType = mimes[url.split('.').pop()] || 'image/jpeg'
+
+		canvas.width = canvas.height = 0
+		ctx.drawImage(image, 0, 0)
+		callback(null, canvas.toDataURL(contentType))
+	})
+
+	image.addEventListener('error', () => {
+		callback(new Error('Failed to load image'))
+	})
+
+	if (!canvas.getContext) {
+		setTimeout(callback, 0, new Error('Canvas is not supported'))
+	}
+}
